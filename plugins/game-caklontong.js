317a2e3e9c3c3f07d15ad813243230c781b55d31
Zeenofc <111058448+Zeenofc@users.noreply.github.com>
2022-08-31 23:08:05 +0800

    Add files via upload

--- plugins/game-caklontong.js
+++ plugins/game-caklontong.js
@@ -1,23 +1,29 @@
-import { caklontong } from '@bochilteam/scraper'
+let fetch = require('node-fetch')
 
 let timeout = 120000
-let poin = 4999
+let poin = 500
+let src
 let handler = async (m, { conn, usedPrefix }) => {
     conn.caklontong = conn.caklontong ? conn.caklontong : {}
     let id = m.chat
-    if (id in conn.caklontong) return conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
-    let json = await caklontong()
+    if (id in conn.caklontong) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
+        throw false
+    }
+    if (!src) src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/caklontong.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
 ${json.soal}
+
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}calo untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.caklontong[id] = [
-        await conn.sendButton(m.chat, caption, author, null, [['Bantuan', `${usedPrefix}calo`]], m),
+        await conn.sendBut(m.chat, caption, author, 'Bantuan', '.calo', m),
         json, poin,
         setTimeout(async () => {
-            if (conn.caklontong[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*\n${json.deskripsi}`, author, null, [['Cak Lontong', `${usedPrefix}caklontong`]], conn.caklontong[id][0])
+            if (conn.caklontong[id]) conn.sendBut(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*\n${json.deskripsi}`, wm, 'Cak Lontong', '.caklontong', conn.caklontong[id][0])
             delete conn.caklontong[id]
         }, timeout)
     ]
@@ -26,4 +32,4 @@ handler.help = ['caklontong']
 handler.tags = ['game']
 handler.command = /^caklontong/i
 
-export default handler
+module.exports = handler
